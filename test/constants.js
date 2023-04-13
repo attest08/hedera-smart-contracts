be5cd9786a07a7b53a19e72389681422ec18b4b3
Aleksandrina Aleksieva-Todorova <aleksandrina.aleksieva@limechain.tech>
2023-04-13 09:15:47 +0300

    tests: resolve comments from PR
    Signed-off-by: Aleksandrina Aleksieva-Todorova <aleksandrina.aleksieva@limechain.tech>

--- test/constants.js
+++ test/constants.js
@@ -1,3 +1,23 @@
+/*-
+ *
+ * Hedera Smart Contracts
+ *
+ * Copyright (C) 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 const Events = {
     Success: "success",
     ResponseCode: "ResponseCode",
@@ -23,16 +43,9 @@ const Events = {
     TokenInfoEvent: "TokenInfoEvent",
     FungibleTokenInfoEvent: "FungibleTokenInfoEvent",
     NftMinted: "NftMinted",
-    ResponseCode: "ResponseCode",
-    Frozen: "Frozen",
-    TokenInfo: "TokenInfo",
-    TokenKey: "TokenKey",
     PausedToken: "PausedToken",
     UnpausedToken: "UnpausedToken",
-    KycGranted: "KycGranted",
-    TokenExpiryInfo: "TokenExpiryInfo",
     ResponseCode: "ResponseCode",
-    FungibleTokenInfo: "FungibleTokenInfo",
     CreatedToken: "CreatedToken",
     MintedToken: "MintedToken",
     CallResponseEvent: "CallResponseEvent",
@@ -72,13 +85,13 @@ const Contract = {
     SafeHTS: "SafeHTS",
 }
 
-CALL_EXCEPTION = "CALL_EXCEPTION",
-    GAS_LIMIT_1_000_000 = { gasLimit: 1_000_000 },
-    GAS_LIMIT_10_000_000 = { gasLimit: 10_000_000 },
-    GAS_LIMIT_800000 = { gasLimit: 800000 },
-    GAS_LIMIT_8000000 = { gasLimit: 8000000 },
-    TOKEN_NAME= "tokenName",
-    TOKEN_SYMBOL= "tokenSymbol",
+const CALL_EXCEPTION = "CALL_EXCEPTION";
+const GAS_LIMIT_1_000_000 = { gasLimit: 1_000_000 };
+const GAS_LIMIT_10_000_000 = { gasLimit: 10_000_000 };
+const GAS_LIMIT_800000 = { gasLimit: 800000 };
+const GAS_LIMIT_8000000 = { gasLimit: 8000000 };
+const TOKEN_NAME = "tokenName";
+const TOKEN_SYMBOL = "tokenSymbol";
 
 module.exports = {
     Events, Path, Contract, CALL_EXCEPTION, GAS_LIMIT_1_000_000, GAS_LIMIT_10_000_000, GAS_LIMIT_800000, GAS_LIMIT_8000000, TOKEN_NAME, TOKEN_SYMBOL
